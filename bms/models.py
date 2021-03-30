22d013871a7a44302ee487dd0844225712e73d56
cyx0706 <3519161997@qq.com>
2021-03-30 21:30:12 +0800

    初始化项目

---
+++ bms/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=12, verbose_name="学号", unique=True)
+    student_name = models.CharField(max_length=12, verbose_name="姓名")
+    borrow_max = models.PositiveIntegerField(verbose_name="最大借阅数")
+    borrow_now = models.IntegerField(verbose_name="已借阅数")
+
+
+class Book(models.Model):
+    book_name = models.CharField(max_length=256, verbose_name="书名")
+    author = models.CharField(max_length=256, verbose_name="作者")
+    press = models.CharField(max_length=256, verbose_name="出版社")
+    borrow_status = models.BooleanField(verbose_name="借阅状态")
+
+
+class BorrowLog(models.Model):
+    state = (
+        (0, "预约"),
+        (1,  "已借阅"),
+        (2, "已归还")
+    )
+
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    status = models.SmallIntegerField(verbose_name="借阅状态", choices=state, default=0)
+    order_time = models.DateField(verbose_name="预约时间")
+    borrow_time = models.DateField(verbose_name="借阅时间")
+    giveback_time = models.DateField(verbose_name="归还时间")
+
+
+class Manager(models.Model):
+    manager_id = models.CharField(max_length=12, verbose_name="工号")
+    manager_name = models.CharField(max_length=12, verbose_name="管理员姓名")
+    authority = models.SmallIntegerField(verbose_name="权限等级")
